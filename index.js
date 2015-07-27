a77569e8d660aeb49c44e2f11659692b14b76ce6
Mr-YS <lucky.yoon0819@gmail.com>
2015-07-27 18:37:48 +0900

    Make server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world!');
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res) {
+  res.send(
+    '<!doctype html>' +
+    '<html>' +
+    '<body>' +
+    '<h1>Hello, world!</h1>' +
+    '<p>장차 내 agario 서버가 될 페이지</p>' +
+    '</body>' +
+    '</html>'
+  );
+});
+
+
+
+app.listen(3000);
